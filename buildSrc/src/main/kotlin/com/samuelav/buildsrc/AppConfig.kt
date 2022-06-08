9b09734abced5808ace94996b18f944b03e333c8
Samuel-Ambrosio <samuel.ambrosiovazquez@gmail.com>
2022-06-08 19:13:28 +0200

    Version up

--- buildSrc/src/main/kotlin/com/samuelav/buildsrc/AppConfig.kt
+++ buildSrc/src/main/kotlin/com/samuelav/buildsrc/AppConfig.kt
@@ -3,7 +3,7 @@ package com.samuelav.buildsrc
 object AppConfig {
     const val applicationId = "com.samuelav.weatherates"
     const val versionCode = 1
-    const val versionName = "0.0.1"
+    const val versionName = "0.0.2"
     const val compileSdkVersion = 32
     const val targetSdkVersion = compileSdkVersion
     const val minSdkVersion = 23
